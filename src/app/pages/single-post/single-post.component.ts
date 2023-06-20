534275a60825f31ab5f753c646acbbc7f7637903
Vatibor <vatibor2@gmail.com>
2023-06-20 22:51:03 +0200

    # single post page side panel shows similarly post

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PostsService} from "../../services/posts.service";
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.css']
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit{
+  postData: any
+  similarPostArray: Array<object> | undefined
 
+
+  constructor(private route: ActivatedRoute, private postService: PostsService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(val => {
+      this.postService.loadOnePost(val['id']).subscribe(post => {
+        // console.log(post)
+        this.postData = post
+        this.loadSimilarPost(this.postData.category.categoryId)
+      })
+    })
+  }
+
+  loadSimilarPost( catId: string){
+    this.postService.loadSimilar(catId).subscribe(val => {
+      this.similarPostArray = val
+    })
+  }
 }
